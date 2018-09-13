5eae900d62c122fbae6b948389d13b65866bc397
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-09-13 12:34:28 -0300

    Update vala.c

--- vala.c
+++ vala.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct No{
+    int numero;
+    struct No *esquerda;
+    struct No *direita;
+};
+typedef struct No No;
+
+void insercao (No **pRaiz, int numero2){
+    if (*pRaiz==NULL)
+    {
+        *Raiz=(No *)malloc(sizeof(No));
+        (*pRaiz) -> esquerda=NULL;
+        (*pRaiz) -> direita=NULL;
+        (*pRaiz) -> numero=numero2;
+    }
+    else
+    { if (numero2 <((*pRaiz)->numero))
+    {
+        insercao(&((*pRaiz)->direita),numero2);
+    }
+
+    }
+}
+void exibirEmOrdem(No *pRaiz){
+    if(pRaiz != NULL){
+        exibirEmOrdem(pRaiz->Esquerda);
+        printf("\n%i",pRaiz->numero);
+        exibirEmOrdem(pRaiz->direita);
+    }
+}
+void exibirPreOrdem(No *pRaiz){
+    if(pRaiz != NULL){
+        printf("\n%d",pRaiz->numero);
+        exibirPreOrdem(pRaiz->esquerda);
+        exibirPreOrdem(pRaiz->direita);
+    }
+}
+
+void exibirPosOrdem(No *pRaiz){
+    if(pRaiz != NULL){
+        exibirPosOrdem(pRaiz->esquerda);
+        exibirPosOrdem(pRaiz->direita);
+        printf("\n%i",pRaiz->numero);
+    }
+}
 int main(void){
 
-return 0;
+
+
+    return 0;
 }
